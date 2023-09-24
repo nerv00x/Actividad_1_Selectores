44932d0165f733ec564f5f4089f691816a4ac4b0
Nerv00x <aridanecabreraramirez@gmail.com>
2023-09-25 00:04:29 +0100

    Terminado_1

--- js/changeSize.js
+++ js/changeSize.js
@@ -5,30 +5,30 @@ function CambiarTamaño(valor) {
     let selector = document.querySelector("#select-parrafo");
     let parrafo = document.querySelector(`#parrafo${selector.value}`)
     console.log(parrafo)
-    if(valor == "grande"){
+    if (valor == "grande") {
         parrafo.setAttribute("class", "grande")
-    }else if(valor == "normal"){
+    } else if (valor == "normal") {
         parrafo.setAttribute("class", "normal")
-    }else {
+    } else {
         parrafo.setAttribute("class", "pequeno")
-}
+    }
 }
 
 function CambiarColor(valor) {
     let selector = document.querySelector("#select-parrafo");
     let parrafo = document.querySelector(`#parrafo${selector.value}`)
     console.log(parrafo)
-    if(valor == "verde"){
+    if (valor == "verde") {
         parrafo.classList.remove("rojo")
         parrafo.classList.remove("negro")
         parrafo.classList.add("verde")
-    }else if(valor == "rojo"){
+    } else if (valor == "rojo") {
         parrafo.classList.remove("verde")
         parrafo.classList.remove("negro")
-        parrafo.classList.add( "rojo")
-    }else {
+        parrafo.classList.add("rojo")
+    } else {
         parrafo.classList.remove("rojo")
         parrafo.classList.remove("verde")
         parrafo.classList.add("negro")
-}
+    }
 }
